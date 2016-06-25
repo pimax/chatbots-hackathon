bcde43810cf8eda82b7f5447323d5c23e914c271
pimax <m.pinyugin@gmail.com>
2016-06-25 17:49:58 +0300

    fixed

--- webhook.php
+++ webhook.php
@@ -9,6 +9,7 @@ require 'app/models/Tour.php';
 require 'app/models/Member.php';
 require 'app/models/DepartureCity.php';
 require 'app/models/Countries.php';
+require 'app/models/Session.php';
 require 'app/models/Subscription.php';
 
 $app = new app\App();
